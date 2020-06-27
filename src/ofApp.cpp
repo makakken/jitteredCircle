9b676ee3b8ef3c46f6b53d1327f8b199cc808d38
Stephan Kropop <gooze@Stephans-MacBook-Air.local>
2020-06-27 20:01:05 +0200

    Working Example

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -2,7 +2,14 @@
 
 //--------------------------------------------------------------
 void ofApp::setup(){
-
+     ofBackground(128);
+     
+     gui.setup();
+     gui.add(diameter.set("Element Diameter",70,1,500));
+     gui.add(jitter.set("Jitter",.15,.01,1));
+     gui.add(jitter_percent.set("JitterPercent",0.001,0,1));
+     
+     capture = false;
 }
 
 //--------------------------------------------------------------
@@ -12,12 +19,31 @@ void ofApp::update(){
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-
+    if(capture) {
+        ofBeginSaveScreenAsSVG("screenshot.svg");
+    }
+    ofEnableAlphaBlending();
+    ofBackground(0);
+    ofSetColor( ofColor(255));
+    for(auto &line : lines) {
+        line.draw();
+    }
+    if(capture) {
+        ofEndSaveScreenAsSVG();
+        capture = false;
+    }
+    gui.draw();
 }
 
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
-
+    if(key == ' ') { capture = true; }
+    if(key == 'c') {
+        lines.clear();
+    }
+    if(key == 'a') {
+        lines.push_back(noiseCircle(300, 300, diameter, 24));
+    }
 }
 
 //--------------------------------------------------------------
@@ -68,4 +94,24 @@ void ofApp::gotMessage(ofMessage msg){
 //--------------------------------------------------------------
 void ofApp::dragEvent(ofDragInfo dragInfo){ 
 
+}
+
+//--------------------------------------------------------------
+ofPolyline ofApp::noiseCircle(int cx, int cy, int r, int num) {
+    ofPolyline line;
+    float a = TWO_PI / num;
+    for (int i = 0; i < num; i++) {
+        float rand = ofRandom(-1 * jitter, jitter);
+        int x = r * (1-rand) * cos(a*(i+rand));
+        int y = r * (1-rand) * sin(a*(i+rand));
+        ofVec3f v = ofVec3f(x,y,0);
+        // line.addVertex(v);
+        line.curveTo(v);
+    }
+    line.close();
+    float s = ofRandom(0,jitter_percent) + 1;
+    line.scale(s,s-0.01);
+    line.rotateDeg(45, ofVec3f(0,0,1));
+    line.translate(ofVec3f(cx,cy,0));
+    return line;
 }
--- src/ofApp.h
+++ src/ofApp.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "ofMain.h"
+#include "ofxGui.h"
 
 class ofApp : public ofBaseApp{
 
@@ -8,6 +9,17 @@ class ofApp : public ofBaseApp{
 		void setup();
 		void update();
 		void draw();
+    
+        ofPolyline noiseCircle(int cx, int cy, int r, int num);
+    
+        vector <ofPolyline> lines;
+    
+        ofParameter<int> diameter;
+        ofParameter<float> jitter;
+        ofParameter<float> jitter_percent;
+    
+        ofxPanel gui;
+        bool capture;
 
 		void keyPressed(int key);
 		void keyReleased(int key);
